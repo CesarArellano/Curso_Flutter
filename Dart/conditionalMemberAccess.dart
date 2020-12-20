649e43e95d220c813a98b143d668b8335d86e7e0
CesarArellano <raywayday@gmail.com>
2020-12-20 17:40:07 -0600

    Conditional Member Access

---
+++ Dart/conditionalMemberAccess.dart
@@ -0,0 +1,17 @@
+void main (){
+  //Conditional member access ?
+  
+  Hero name = new Hero("Spiderman");
+  // Si es null el objeto, no se llama el método printName.
+  name?.printName();
+  print("fin");
+}
+
+class Hero {
+  final String _name;
+  Hero(this._name);
+  
+  printName(){
+    print(_name);
+  }
+}

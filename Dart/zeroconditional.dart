6c327a443594e4a18ac0f1573894469d01907496
CesarArellano <raywayday@gmail.com>
2020-12-20 17:32:50 -0600

    Zero and Null Conditional

---
+++ Dart/zeroconditional.dart
@@ -0,0 +1,18 @@
+void main (){
+  int age = 17;
+  // Zero coditional
+  String text = age >= 18 ? "Mayor de edad" : "Menor de Edad";
+  print(text);
+  
+  // Null coditional
+  String name = "Name isn't null";
+  //Si es null name tomará el texto "No definido".
+  String result = name ?? "No Definido";
+  print(result);
+  printText(name??"Es null");
+  printText(name!=null ? name : "Es null");
+}
+
+void printText(String text){
+  print(text);
+}

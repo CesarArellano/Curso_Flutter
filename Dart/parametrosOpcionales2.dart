631924265e3a43c317e662c69a5f5ca011faa630
CesarArellano <raywayday@gmail.com>
2020-12-23 18:43:46 -0600

    Mi Primera App

--- Dart/parametrosOpcionales2.dart
+++ Dart/parametrosOpcionales2.dart
@@ -1,10 +1,11 @@
 void main() {
   printTextWithNumber("My age", age:20, hasJob:true);
 }
-​
+
 void printTextWithNumber(String text, { int age = 0, bool hasJob}){
   print("$text: $age");
   
   if(hasJob != null && hasJob == true){
     print("Good");
   }
+}

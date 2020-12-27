10d65864e7830d45fb805780b3b061fa732f1945
CesarArellano <raywayday@gmail.com>
2020-12-27 13:29:25 -0600

    Checkbox, CheckboxListTile y SwitchListTile

--- Flutter/componentes/lib/src/pages/slider_page.dart
+++ Flutter/componentes/lib/src/pages/slider_page.dart
@@ -7,6 +7,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _valorslider = 100.0;
+  bool _bloquearSlider = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +19,8 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _crearSlider(),
+            _checkBox(),
+            _crearSwitch(),
             Expanded(child: _crearImagen()),
           ],
         ),
@@ -32,11 +35,13 @@ class _SliderPageState extends State<SliderPage> {
       min: 10.0,
       max: 400.0,
       value: _valorslider,
-      onChanged: (valor) {
-        setState(() {
-          _valorslider = valor;
-        });
-      },
+      onChanged: (_bloquearSlider)
+          ? null
+          : (valor) {
+              setState(() {
+                _valorslider = valor;
+              });
+            },
     );
   }
 
@@ -46,6 +51,41 @@ class _SliderPageState extends State<SliderPage> {
           'https://static.wikia.nocookie.net/vsbattles/images/4/4c/WiiU_Wind_Waker_Link.png/revision/latest?cb=20151115121618'),
       width: _valorslider,
       fit: BoxFit.contain,
+    );
+  }
+
+  Widget _checkBox() {
+    /*
+    return Checkbox(
+      value: _bloquearSlider,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearSlider = valor;
+        });
+      },
+    );
+    */
+    return CheckboxListTile(
+      title: Text('Bloquear slider'),
+      value: _bloquearSlider,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearSlider = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      activeColor: Colors.green,
+      title: Text('Bloquear slider'),
+      value: _bloquearSlider,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearSlider = valor;
+        });
+      },
     );
   }
 }

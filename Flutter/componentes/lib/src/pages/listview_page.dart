6e8129140b982396bcc2e541c3e55169edb02a57
CesarArellano <raywayday@gmail.com>
2020-12-27 13:59:16 -0600

    InfiniteScroll

--- Flutter/componentes/lib/src/pages/listview_page.dart
+++ Flutter/componentes/lib/src/pages/listview_page.dart
@@ -6,7 +6,24 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // Método de inicializa la clase
+    super.initState();
+    _agregarDiez();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregarDiez();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +37,7 @@ class _ListaPageState extends State<ListaPage> {
   Widget _crearLista() {
     //Elementos de lista indefinidos / ilimitados, serán renderizados en tiempo real.
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
@@ -29,5 +47,13 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+  void _agregarDiez() {
+    for (int i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
   }
 }

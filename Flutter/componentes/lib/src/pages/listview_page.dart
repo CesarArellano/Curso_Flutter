cc404aba030f196caad714cdbe0cc93bc2704699
CesarArellano <raywayday@gmail.com>
2020-12-27 14:26:04 -0600

    InfiniteScroll con Future

--- Flutter/componentes/lib/src/pages/listview_page.dart
+++ Flutter/componentes/lib/src/pages/listview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ListaPage extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class _ListaPageState extends State<ListaPage> {
 
   List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -17,21 +19,33 @@ class _ListaPageState extends State<ListaPage> {
     super.initState();
     _agregarDiez();
     _scrollController.addListener(() {
+      //Traemos más imágenes cuando llegue el usuario a la última imagen cargada.
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        _agregarDiez();
+        fetchData();
       }
     });
   }
 
   @override
+  void dispose() {
+    // Desechar la pantalla.
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Listas'),
-      ),
-      body: _crearLista(),
-    );
+        appBar: AppBar(
+          title: Text('Listas'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _crearLista(),
+            _crearLoading(),
+          ],
+        ));
   }
 
   Widget _crearLista() {
@@ -44,6 +58,8 @@ class _ListaPageState extends State<ListaPage> {
         return FadeInImage(
           image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
           placeholder: AssetImage('assets/jar-loading.gif'),
+          height: 300,
+          fit: BoxFit.cover,
         );
       },
     );
@@ -55,5 +71,45 @@ class _ListaPageState extends State<ListaPage> {
       _listaNumeros.add(_ultimoItem);
     }
     setState(() {});
+  }
+
+  Future fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, respuestaHTTP);
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 150,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(milliseconds: 250),
+    );
+    _agregarDiez();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(
+                backgroundColor: Colors.blue,
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.0),
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }

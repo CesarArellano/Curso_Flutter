bcd120f12676f90bc48098bb23400f7392e063cf
CesarArellano <raywayday@gmail.com>
2020-12-29 13:28:45 -0600

    Creando Stream de peliculas

--- Flutter/peliculas/lib/src/providers/peliculas_provider.dart
+++ Flutter/peliculas/lib/src/providers/peliculas_provider.dart
@@ -1,12 +1,31 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:peliculas/src/models/peliculas_model.dart';
 
 class PelicularProvider {
   String _apikey = '62e55e873b3d4db86782226591ef48ae';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+  //Todos van a escuchar el Stream por el .broadcast
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
+
+  //Podríamos quitar Function(List<Pelicula>) pero queremos asegurarnos que sea una lista de tipo Película.
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  //Necesario cerrar Streams.
+  void disposeStreams() {
+    _popularesStreamController.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -27,10 +46,17 @@ class PelicularProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key': _apikey,
       'language': _language,
+      'page': _popularesPage.toString(),
     });
-    return _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    return resp;
   }
 }

18f77c6831bdb97255a99f611c1df3ae66612c4a
CesarArellano <raywayday@gmail.com>
2020-12-29 23:01:13 -0600

    SearchDelegate, BuildActions y BuildLeading

--- Flutter/peliculas/lib/src/pages/home_page.dart
+++ Flutter/peliculas/lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/search/search_delegate.dart';
 import 'package:peliculas/src/widgetsPersonalizados/card_swiper_widget.dart';
 import 'package:peliculas/src/widgetsPersonalizados/movie_horizontal.dart';
 
@@ -18,7 +19,9 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
           )
         ],
       ),
---
+++ Flutter/peliculas/lib/src/search/search_delegate.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro AppBar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del AppBar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Es el autocompletado de la búsqueda.
+    return Container();
+  }
+}

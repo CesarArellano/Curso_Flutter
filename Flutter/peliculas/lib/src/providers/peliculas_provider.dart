be1d0f709f0fb92a7f890f64f1aabec46b73379f
CesarArellano <raywayday@gmail.com>
2020-12-29 20:06:54 -0600

    Servicio para obtener los actores de la película

--- Flutter/peliculas/lib/src/providers/peliculas_provider.dart
+++ Flutter/peliculas/lib/src/providers/peliculas_provider.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 import 'package:peliculas/src/models/peliculas_model.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 
 class PelicularProvider {
   String _apikey = '62e55e873b3d4db86782226591ef48ae';
@@ -63,5 +64,17 @@ class PelicularProvider {
 
     _cargando = false;
     return resp;
+  }
+
+  Future<List<Actor>> getCast(String idPeli) async {
+    final url = Uri.https(_url, '3/movie/$idPeli/credits', {
+      'api_key': _apikey,
+      'language': _language,
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+    return cast.actores;
   }
 }

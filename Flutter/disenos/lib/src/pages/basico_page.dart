3dbfe2b3f1f0c8a500ae59267cdd50a716aae57a
CesarArellano <raywayday@gmail.com>
2021-01-04 13:20:08 -0600

    Diseño básico - Parte 3

--- Flutter/disenos/lib/src/pages/basico_page.dart
+++ Flutter/disenos/lib/src/pages/basico_page.dart
@@ -7,45 +7,59 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-      ],
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _crearImagen(),
+          _crearTitulo(),
+          SizedBox(height: 10.0),
+          _crearAcciones(),
+          SizedBox(height: 10.0),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+      ),
     ));
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          'https://lh3.googleusercontent.com/qsm1GPWNVY0D2IADaRmDKkQrOmZeDahaAOjuHcW7iZ8XTTJiFNj5nfSa8-2D4RGewn7av_FpEipYlCMQFoQsGjz3=w640-h400-e365-rj-sc0x00ffffff'),
-      fit: BoxFit.cover,
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://lh3.googleusercontent.com/qsm1GPWNVY0D2IADaRmDKkQrOmZeDahaAOjuHcW7iZ8XTTJiFNj5nfSa8-2D4RGewn7av_FpEipYlCMQFoQsGjz3=w640-h400-e365-rj-sc0x00ffffff'),
+        height: 180.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Paisaje Lago', style: estiloTitulo),
-                SizedBox(
-                  height: 7.0,
-                ),
-                Text('Un lago que se encuentra en Alemania',
-                    style: estiloSubtitulo),
-              ],
+    return SafeArea(
+      top: false,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Paisaje Lago', style: estiloTitulo),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text('Un lago que se encuentra en Alemania',
+                      style: estiloSubtitulo),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0),
-          Text('41', style: TextStyle(fontSize: 20.0)),
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0)),
+          ],
+        ),
       ),
     );
   }
@@ -76,7 +90,7 @@ class BasicoPage extends StatelessWidget {
 
   Widget _crearTexto() {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
       child: Text(
         'It has been more than a century since the Ashokan Reservoir was put into service, and more than 30 years since completion of the Cannonsville Reservoir, the last in the series of water infrastructures on the Catskill and Delaware watersheds in upstate New York.',
         textAlign: TextAlign.justify,

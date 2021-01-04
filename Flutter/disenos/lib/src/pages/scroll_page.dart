435cf679547b90f1ebdc8a4ebcf4669e8b78d2e3
CesarArellano <raywayday@gmail.com>
2021-01-04 13:41:12 -0600

    Diseño con scroll - parte 2

--- Flutter/disenos/lib/src/pages/scroll_page.dart
+++ Flutter/disenos/lib/src/pages/scroll_page.dart
@@ -15,14 +15,55 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _paginaUno() {
-    return Center(
-      child: Text('Página 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
 
   Widget _paginaDos() {
     return Center(
       child: Text('Página 2'),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        child:
+            Image(image: AssetImage('assets/scroll-1.png'), fit: BoxFit.cover));
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0),
+          Text(
+            '11°',
+            style: estiloTexto,
+          ),
+          Text(
+            'Miércoles',
+            style: estiloTexto,
+          ),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+        ],
+      ),
     );
   }
 }

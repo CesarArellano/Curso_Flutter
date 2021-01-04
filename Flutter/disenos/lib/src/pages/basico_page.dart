4e5393a8add76c9bcd55f306913d353b3a0c9a88
CesarArellano <raywayday@gmail.com>
2021-01-04 12:28:28 -0600

    Diseño básico - parte 2

--- Flutter/disenos/lib/src/pages/basico_page.dart
+++ Flutter/disenos/lib/src/pages/basico_page.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  final estiloSubtitulo = TextStyle(fontSize: 16.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Básico Page'),
+        body: Column(
+      children: <Widget>[
+        Image(
+          image: NetworkImage(
+              'https://lh3.googleusercontent.com/qsm1GPWNVY0D2IADaRmDKkQrOmZeDahaAOjuHcW7iZ8XTTJiFNj5nfSa8-2D4RGewn7av_FpEipYlCMQFoQsGjz3=w640-h400-e365-rj-sc0x00ffffff'),
+          fit: BoxFit.cover,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Paisaje Lago', style: estiloTitulo),
+                    SizedBox(
+                      height: 7.0,
+                    ),
+                    Text('Un lago que se encuentra en Alemania',
+                        style: estiloSubtitulo),
+                  ],
+                ),
+              ),
+              Icon(Icons.star, color: Colors.red, size: 30.0),
+              Text('41', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        ),
+      ],
     ));
   }
 }

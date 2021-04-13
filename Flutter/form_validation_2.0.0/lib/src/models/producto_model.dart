80f1f9e497bbbee8ed8e20e8960802c44de6872f
CesarArellano <raywayday@gmail.com>
2021-04-13 13:57:25 -0500

    feat: creando modelo producto

---
+++ Flutter/form_validation_2.0.0/lib/src/models/producto_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+ProductoModel productoModelFromJson(String str) => ProductoModel.fromJson(json.decode(str));
+
+String productoModelToJson(ProductoModel data) => json.encode(data.toJson());
+
+class ProductoModel {
+    ProductoModel({
+      this.id,
+      this.titulo = '',
+      this.valor = 0.0,
+      this.disponible = true,
+      this.fotoUrl,
+    });
+
+    String id;
+    String titulo;
+    double valor;
+    bool disponible;
+    String fotoUrl;
+
+    factory ProductoModel.fromJson(Map<String, dynamic> json) => ProductoModel(
+        id          : json["id"],
+        titulo      : json["titulo"],
+        valor       : json["valor"],
+        disponible  : json["disponible"],
+        fotoUrl     : json["fotoUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "titulo": titulo,
+        "valor": valor,
+        "disponible": disponible,
+        "fotoUrl": fotoUrl,
+    };
+}

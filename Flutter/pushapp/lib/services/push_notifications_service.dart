ce8412fd969a26b120abc9f01906402dfed2f9e1
CesarArellano <raywayday@gmail.com>
2021-05-01 20:23:11 -0500

    Escuchar notificaciones - onBackground, onMessage on OpenedApp

--- Flutter/pushapp/lib/services/push_notifications_service.dart
+++ Flutter/pushapp/lib/services/push_notifications_service.dart
@@ -5,11 +5,29 @@ class PushNotificationService {
   static FirebaseMessaging messsaging = FirebaseMessaging.instance;
   static String? token;
 
+  static Future _onBackgroundHandler(RemoteMessage message) async {
+    print('onBackground Handler ${ message.messageId }');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('onMessage Handler ${ message.messageId }');
+  }
+
+  static Future _onMessageOpenHandler(RemoteMessage message) async {
+    print('onMessageOpen Handler ${ message.messageId }');
+  }
+
   static Future initializeApp() async {
     // Push Notifications
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
-    print(token);
-    // Local Notifications
+    print('Token: $token');
+    
+    // Handlers (Escuchando notificaciones)
+    FirebaseMessaging.onBackgroundMessage(_onBackgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenHandler);
   }
 }
+
+// eAKR8LoDTaGlnRN0FTGCSD:APA91bGOVkdJTAMG5iO9MWzwcEH8yG3SEDChKTpn9yWQiQ6Zkw02zXTIQvxvkKE2VAW8c7N-U8rt-_RGu5pqp13yTw_gYfZpLzgk3qqxVaZwDrWcxipYNlTpE7Hy_dTn7CuKfJUGKNZz

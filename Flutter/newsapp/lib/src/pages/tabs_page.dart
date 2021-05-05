2fb8058fae5a56452381b493b883f0df4693e9b0
CesarArellano <raywayday@gmail.com>
2021-05-05 13:52:50 -0500

    Adding BottomNavigationBar in TabsPage

--- Flutter/newsapp/lib/src/pages/tabs_page.dart
+++ Flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -5,17 +5,46 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(
-            color: Colors.red
-          ),
-          Container(
-            color: Colors.green
-          ),
-        ]
-      )
+      body: _Pages(),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: BouncingScrollPhysics()
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red
+        ),
+        Container(
+          color: Colors.green
+        ),
+      ]
+    );
+  }
+}
+
+class NavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon( Icons.person_outline ),
+          label: 'Para ti'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.public ),
+          label: 'Encabezados'
+        ),
+      ],
     );
   }
 }

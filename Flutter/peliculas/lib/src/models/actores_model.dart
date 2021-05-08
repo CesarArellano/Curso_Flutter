c9360a7beef3a881773eb4850d37d2e51152f98d
CesarArellano <raywayday@gmail.com>
2021-05-07 19:03:32 -0500

    Before migrating

--- Flutter/peliculas/lib/src/models/actores_model.dart
+++ Flutter/peliculas/lib/src/models/actores_model.dart
@@ -2,8 +2,6 @@
 class Cast {
   List<Actor> actores = [];
   Cast.fromJsonList(List<dynamic> jsonList) {
-    if (jsonList == null) return;
-
     jsonList.forEach((item) {
       final actor = new Actor.fromJsonMap(item);
       actores.add(actor);

1118531dab833b99ab4f5cb27b3cb3c227de1472
CesarArellano <raywayday@gmail.com>
2021-05-20 21:23:22 -0500

    Adding empty image and text

--- Flutter/qr_reader/lib/widgets/scan_tiles.dart
+++ Flutter/qr_reader/lib/widgets/scan_tiles.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_reader/models/scan_model.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
 import 'package:qr_reader/utils/utils.dart';
 
@@ -10,10 +12,50 @@ class ScanTiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final widthDevice = MediaQuery.of(context).size.width;
     final scanListProvider = Provider.of<ScanListProvider>(context);
     final scans = scanListProvider.scans;
     
+    return (scans.length > 0) 
+      ? ScansList(scans: scans, tipo: tipo)
+      : EmptyScans(widthDevice: widthDevice);
+  }
+}
+
+class EmptyScans extends StatelessWidget {
+  EmptyScans({ @required this.widthDevice });
+
+  final double widthDevice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 50),
+          child: SvgPicture.asset('assets/svgs/empty.svg', width: widthDevice * 0.9)
+        ),
+        Text('No hay scans disponibles', style: TextStyle(fontSize: 18.0),)
+      ],
+    );
+  }
+}
+
+class ScansList extends StatelessWidget {
+  final List<ScanModel> scans;
+  final String tipo;
+  
+  ScansList({
+    @required this.scans,
+    @required this.tipo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.builder(
+      physics: BouncingScrollPhysics(),
       itemCount: scans.length,
       itemBuilder: ( _, i ) => Dismissible(
         key: UniqueKey(),

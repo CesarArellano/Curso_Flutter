ed76c904a3260de944911e133c7040b7ea7281cb
CesarArellano <raywayday@gmail.com>
2021-12-14 18:52:51 -0600

    Renewing the movie app

---
+++ Flutter/movie_app/lib/screens/details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  
+  const DetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Details World!'),
+      ),
+    );
+  }
+}
---
+++ Flutter/movie_app/lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:movie_app/screens/details_screen.dart';
+export 'package:movie_app/screens/home_screen.dart';

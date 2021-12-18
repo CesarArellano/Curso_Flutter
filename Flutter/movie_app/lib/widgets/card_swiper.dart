3a0fe4acc3d7bbfe5a770688cd04d871f2e22d63
CesarArellano <raywayday@gmail.com>
2021-12-17 22:05:14 -0600

    Adding CardSwiper Package

--- Flutter/movie_app/lib/widgets/card_swiper.dart
+++ Flutter/movie_app/lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,34 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
-      width: double.infinity,
-      height: 225,
-      color: Colors.orangeAccent,
+      padding: const EdgeInsets.only(top: 30),
+      height: size.height * 0.5,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.8,
+        itemCount: 3,
+        itemBuilder: ( _ , int index) {
+          return Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: const FadeInImage(
+                  placeholder: AssetImage('assets/images/loading.gif'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              InkWell(
+                borderRadius: BorderRadius.circular(20),
+                onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+              ),
+            ]
+          );
+        },
+      )
     );
   }
 }

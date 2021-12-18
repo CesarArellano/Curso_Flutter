0081cee7772f9db0def541ea21e01dc437b06744
CesarArellano <raywayday@gmail.com>
2021-12-17 23:07:45 -0600

    Adding PosterAndTitle Widget

--- Flutter/movie_app/lib/screens/details_screen.dart
+++ Flutter/movie_app/lib/screens/details_screen.dart
@@ -6,9 +6,108 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Details World!'),
+    final movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'No movie';
+    debugPrint(movie);
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(movie: movie),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const _PosterAndTitle()
+            ])
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final String movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            movie,
+            style: const TextStyle(fontSize: 18)
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/images/loading.gif'),
+          image: AssetImage('assets/images/loading.gif'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only( top: 20 ),
+      padding: const EdgeInsets.symmetric( horizontal: 20 ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: const FadeInImage(
+              width: 100,
+              height: 150,
+              placeholder: AssetImage('assets/images/loading.gif'),
+              image: AssetImage('assets/images/loading.gif'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Movie Title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'Movie Original Title',
+                style: textTheme.subtitle2,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star, color: Colors.yellow),
+                  const SizedBox(width: 5),
+                  Text('Promedio votos', style: textTheme.caption,)
+                ],
+              )
+
+            ],
+          )
+        ],
       ),
     );
   }
